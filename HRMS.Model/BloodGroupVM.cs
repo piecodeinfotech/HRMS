589beec742d5f7e7f282262a756e355603bc18f6
piecodeinfotech <piecodeinfotech@gmail.com>
2023-01-19 19:24:54 +0530

    addd all files

---
+++ HRMS.Model/BloodGroupVM.cs
@@ -0,0 +1,22 @@
+﻿namespace HRMS.Model
+{
+    public class BloodGroupVM
+    {
+        public int Id { get; set; }
+        public string? BloodGroup_Name { get; set; }
+        public DateTime? CreatedOn_Date { get; set; }
+        public int? CreatedBy_Login_User_Id { get; set; }
+        public int? CreatedBy_Login_Session_Id { get; set; }
+        public string? CreatedFrom_Page { get; set; }
+        public int? CreatedFrom_Sub_Menu_Id { get; set; }
+        public string? CreatedFrom_API_Name { get; set; }
+        public DateTime? UpdatedOn_Date { get; set; }
+        public int? UpdatedBy_Login_User_Id { get; set; }
+        public int? UpdatedBy_Login_Session_Id { get; set; }
+        public string? UpdatedFrom_Page { get; set; }
+        public int? UpdatedFrom_Sub_Menu_Id { get; set; }
+        public string? UpdatedFrom_API_Name { get; set; }
+
+
+    }
+}
